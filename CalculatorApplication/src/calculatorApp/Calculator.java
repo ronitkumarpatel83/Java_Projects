6bbf380de71675af520bea61ceec41ead55f87f7
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2023-09-22 21:31:20 +0530

    [Ronit]Added : Calculator Application

---
+++ CalculatorApplication/src/calculatorApp/Calculator.java
@@ -0,0 +1,76 @@
+package calculatorApp;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.run();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Available operations:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the operation (e.g., +, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = add(num1, num2);
+                break;
+            case '-':
+                result = subtract(num1, num2);
+                break;
+            case '*':
+                result = multiply(num1, num2);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = divide(num1, num2);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    System.exit(1);
+                }
+                break;
+            default:
+                System.out.println("Invalid operator. Please use +, -, *, or /.");
+                System.exit(1);
+        }
+
+        System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+
+        scanner.close();
+    }
+
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
+
